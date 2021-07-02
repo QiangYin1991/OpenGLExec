e81f583fae1d800f76083d9b8cc4eae9eb03d25a
qiangyin <yinqiang@catarc.ac.cn>
2021-07-02 10:04:39 +0800

    1. import glfw library and import glad source 2. create TestGLExec Project to test glfw and glad.

---
+++ TestGLFWEnv/main.cpp
@@ -0,0 +1,54 @@
+#include "glad/glad.h"
+#include <glfw3.h>
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, true);
+	}
+}
+
+int main()
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
+	if (window == nullptr)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+	
+	while (!glfwWindowShouldClose(window))
+	{
+		// input
+		processInput(window);
+		// rendering command
+		glClearColor(.2f, .3f, .3f, 1.f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		// check and call events and swap the buffers 
+		glfwPollEvents();
+		glfwSwapBuffers(window);
+	}
+	glfwTerminate();
+	return 0;
+}
